665d975ae3cdee04b53a76de005c458a2b45f6e6
Sushmitha Hegde <58133063+hegdesushmitha@users.noreply.github.com>
2026-01-18 18:05:23 +0530

    Implement customer details component with form

---
+++ src/app/components/customer-details/customer-details.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomerService } from '../../services/customer.service';
+
+@Component({
+  selector: 'app-customer-details',
+  templateUrl: './customer-details.component.html'
+})
+export class CustomerDetailsComponent implements OnChanges {
+
+  @Input() accountNo!: string;
+  customerForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private service: CustomerService
+  ) {
+    this.customerForm = this.fb.group({
+      name: ['', Validators.required],
+      accountNo: ['', Validators.required],
+      status: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  ngOnChanges() {
+    if (this.accountNo) {
+      this.service.getCustomer(this.accountNo)
+        .subscribe(res => {
+          this.customerForm.patchValue({
+            name: res.customerName,
+            accountNo: res.accountNumber,
+            status: res.status,
+            email: 'test@email.com'
+          });
+        });
+    }
+  }
+}

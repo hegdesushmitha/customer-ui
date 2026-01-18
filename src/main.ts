152d1e93f80646929485d6690dd8d300d24fba6a
Sushmitha Hegde <58133063+hegdesushmitha@users.noreply.github.com>
2026-01-19 00:20:19 +0530

    Refactor main.ts to use App component

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
+import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
-}).catch(err => console.error(err));
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Hello from {{ name }}!</h1>
+    <a target="_blank" href="https://angular.dev/overview">
+      Learn more about Angular
+    </a>
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App, {
+  providers: [
+    provideHttpClient()
+  ]
+});

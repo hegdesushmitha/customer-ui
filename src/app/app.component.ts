a95fdeaef3c3ebd5f6d8b42086a9ae3a90b3d95f
Sushmitha Hegde <58133063+hegdesushmitha@users.noreply.github.com>
2026-01-18 23:53:57 +0530

    Add selectedAccount property to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
+import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    CustomerListComponent,
+    CustomerDetailsComponent,
+    TransactionsComponent
+  ],
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  selectedAccount!: string;
+}

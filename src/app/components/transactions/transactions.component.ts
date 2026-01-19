14a3b6178c4b8240fa7f410321160068f6c2e52e
Sushmitha Hegde <58133063+hegdesushmitha@users.noreply.github.com>
2026-01-19 10:13:24 +0530

    Refactor TransactionsComponent to use inline template

--- src/app/components/transactions/transactions.component.ts
+++ src/app/components/transactions/transactions.component.ts
@@ -1,9 +1,27 @@
 import { Component, Input, OnChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-transactions',
-  templateUrl: './transactions.component.html'
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h3 *ngIf="transactions.length">Last 10 Transactions</h3>
+
+    <table *ngIf="transactions.length" border="1">
+      <tr>
+        <th>ID</th>
+        <th>Amount</th>
+        <th>Type</th>
+      </tr>
+      <tr *ngFor="let t of transactions">
+        <td>{{ t.transactionId }}</td>
+        <td>{{ t.amount }}</td>
+        <td>{{ t.type }}</td>
+      </tr>
+    </table>
+  `
 })
 export class TransactionsComponent implements OnChanges {
 
